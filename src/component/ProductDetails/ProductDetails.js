46f66f89b1aa43954799a824f2d7217b0a4ec19c
narimenmokrani1 <narimenmokrani@gmail.com>
2022-01-09 15:29:10 -0600

    adding main branch

--- src/component/ProductDetails/ProductDetails.js
+++ src/component/ProductDetails/ProductDetails.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
-import style from './ProductDetails.css'
+import style from './ProductDetails.css';
 // import { Link } from 'react-router-dom';
 function ProductDetails(props) {
 	const { id } = useParams();
 	const [product, setProduct] = useState({});
 	useEffect(() => {
-	
 		const url = `https://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
 		fetch(url)
 			.then((res) => res.json())
 			.then((res) => {
-				console.log('Single product:' ,res);
+				console.log('Single product:', res);
 				setProduct(res);
 			});
 	}, []);
@@ -23,9 +22,11 @@ function ProductDetails(props) {
 				<h3>{product.name}</h3>
 				<img src={product.image_link} alt={product.name} />
 				<h3>${product.price}</h3>
-				<button className='productLink-button'>
-					<a href={product.product_link}>Shop for product here</a>
-				</button>
+
+				<a className='productLink-button' href={product.product_link}>
+					Shop for product here
+				</a>
+
 				{/* <button>Select</button> */}
 				<h4>{product.description}</h4>
 			</div>
